ec808d1f90d99802103ed1b08dff388198b4178d
David Liebherr <ambition.aidev@gmail.com>
2023-06-21 22:19:19 -0500

    code changes

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 // timer code
-var timeSecond = 3;
-var timeH = document.getElementById("timer");
 
-displayTime(timeSecond);
 
-const countDown = setInterval(() => {
-  timeSecond--;
-  displayTime(timeSecond);
-  if (timeSecond == 0 || timeSecond < 1) {
-    endCount();
-    clearInterval(countDown);
-  }
-}, 1000);
 
-function displayTime(second) {
-  const min = Math.floor(second / 60);
-  const sec = Math.floor(second % 60);
-  timeH.innerHTML = `
-  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
-  `;
+
+
+// Quiz codes
+var yearAnswer = ["1989","1991","1995","1998"]
+var answers = [a1,a2,a3,a4]
+
+for (var i = 0 ; i < yearAnswer.length; i++) {
+answers[i].innerText = yearAnswer [i]
 }
-
-function endCount() {
-  timeH.innerHTML = "Time out";
-}
-
