0a9f66a46e235a5ec954ff064c5c5ba9bb6ca861
David Liebherr <ambition.aidev@gmail.com>
2023-06-22 18:31:45 -0500

    WIP - updated js code for background-color

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // timer code
 var timer = document.querySelector(".timer")
 var bgColor = document.querySelector("body")
-var count = 10
+var count = 2
 var intervalId
 // Quiz Start Button
 var start = document.querySelector(".start-button")
@@ -15,13 +15,13 @@ start.onclick =  function() {
            if(count=== 0) {
                clearInterval(intervalId)
                timer.innerText = "Time's Up!"
+               bgColor.style = "background-color: red;"
             }
             count--
        }, 1000)
 
 }
 
-resi
 
 
 // Quiz codes
