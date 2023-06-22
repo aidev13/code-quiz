dfd1d8770e3f6497fe88e5e2cc3f94dfcd38ae19
David Liebherr <ambition.aidev@gmail.com>
2023-06-22 17:08:16 -0500

    created timer js countdown code and html button - WIP

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // timer code
-var pTimer = document.querySelector(".timer")
-var seconds = 5
+var timer = document.querySelector(".timer")
 
+// Quiz Start Button
+var start = document.querySelector(".start-button")
 
-setInterval(function() {
-    // console.log('running!')
-    pTimer.innerText = seconds
-    seconds--
-}, 1000)
+start.addEventListener('click', function() {
+    
+        setTimeout(function() {
+           timer.innerHTML = "Time's Up!"
+       }, 3000)
+       
+})
 
-setTimeout(function() {
-    pTimer.innerHTML = "Time's Up!"
-}, 5000)
 
 // Quiz codes
 var yearAnswer = ["1989","1991","1995","1998"]
