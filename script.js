b3f3064595b335b26f5c28e490b77b01b1e24682
David Liebherr <ambition.aidev@gmail.com>
2023-06-21 22:42:25 -0500

    working on timer js code

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // timer code
+var pTimer = document.querySelector(".timer")
+var seconds = 5
 
 
+setInterval(function() {
+    // console.log('running!')
+    pTimer.innerText = seconds
+    seconds--
+}, 1000)
 
-
+setTimeout(function() {
+    pTimer.innerHTML = "Time's Up!"
+}, 5000)
 
 // Quiz codes
 var yearAnswer = ["1989","1991","1995","1998"]
